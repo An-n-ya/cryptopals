47b4442edaa36413a86753a60ec8d8e96d996dad
Annya <annya.host@gmail.com>
2024-03-22 15:37:10 +0800

    add pkcs padding validation

--- src/pkcs.rs
+++ src/pkcs.rs
@@ -15,14 +15,18 @@ pub fn pkcs7unpadding(input: &[u8], k: u8) -> Vec<u8> {
     println!("{input:?} len:{}", input.len());
     let l = input.len();
     let padding_size = *input.last().unwrap();
-    let is_padded = input[l - padding_size as usize..l]
-        .iter()
-        .all(|n| *n == padding_size);
-    if !is_padded {
+    if !is_pkcs7_padding(input, k) {
         return input.to_vec();
     }
     let input = &input[0..l - padding_size as usize];
     input.to_vec()
+}
+pub fn is_pkcs7_padding(input: &[u8], k: u8) -> bool {
+    let l = input.len();
+    let padding_size = *input.last().unwrap();
+    input[l - padding_size as usize..l]
+        .iter()
+        .all(|n| *n == padding_size)
 }
 
 #[cfg(test)]
@@ -35,5 +39,14 @@ mod tests {
         let expected = b"YELLOW SUBMARINE\x04\x04\x04\x04";
         let res = pkcs7padding(input, 20);
         assert_eq!(res, expected);
+        let res = pkcs7unpadding(&res, 20);
+        assert_eq!(res, input);
+
+        let input = b"ICE ICE BABY\x04\x04\x04\x04";
+        assert!(is_pkcs7_padding(input, 16));
+        let input = b"ICE ICE BABY\x05\x05\x05\x05";
+        assert!(!is_pkcs7_padding(input, 16));
+        let input = b"ICE ICE BABY\x01\x02\x03\x04";
+        assert!(!is_pkcs7_padding(input, 16));
     }
 }
